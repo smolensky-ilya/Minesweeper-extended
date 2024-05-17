60007900366107f5293d422f5860d69bf928b2e5
Ilia <ilya@s1q.ru>
2024-05-17 08:05:35 +0300

    inventory work

--- game.py
+++ game.py
@@ -1,6 +1,5 @@
-import random
 import pygame
-from time import sleep
+from classes import Field, Inventory
 
 
 class Game:
@@ -33,11 +32,19 @@ class Game:
         self.first_click = True
         self.menu_text = self.change_menu_text()
 
+        # GAME FEATURES
+        self.player_inventory, self.player_immortality = self.initiate_game_features()
+
         # BUTTON INIT (SIZE and POSITIONS)
         self.menu_button = pygame.Rect(10, 5, 100, 30)
         self.another_button = pygame.Rect(120, 5, 130, 30)
 
         self.game_loop(self.screen)
+
+    def initiate_game_features(self):
+        self.player_immortality = True
+        self.player_inventory = Inventory()
+        return self.player_inventory, self.player_immortality
 
     def game_loop(self, screen):
         running = True
@@ -52,23 +59,25 @@ class Game:
                     else:
                         field_x, field_y = x // self.cell_size, (y-self.menu_height) // self.cell_size
                         if event.button == 1:  # LEFT MOUSE BUTTON
-                            #print(f"Clicked: {field_x}, {field_y}")
-
                             # Saving ourselves on the first click - it will recreate the field until safe.
                             while self.first_click and self.field[field_y][field_x].if_bomb:
-                                #print('FIRST CLICK - RECREATING')
                                 self.obtain_a_new_field()
                             self.first_click = False  # It's no longer the first click!
                             if not self.field[field_y][field_x].is_flagged:  # If not flagged - OPEN
                                 self.field_object.open_tile(x=field_x, y=field_y, draw_object=self.draw_the_field,
                                                             screen=screen)
                             if self.check_if_we_click_on_a_bomb(field_x, field_y):
-                                self.game_over()
+                                if self.player_immortality:  # Handing immortality logic
+                                    self.player_immortality = False
+                                    self.field_object.open_tile(x=field_x, y=field_y, draw_object=self.draw_the_field,
+                                                                screen=screen)
+                                else:
+                                    self.game_over()
+
                             else:
                                 self.change_menu_text()  # updating the number of remaining tiles to open
                             self.first_click = False
                         elif event.button == 3:  # RIGHT MOUSE BUTTON
-                            #print('RMB')
                             self.field[field_y][field_x].get_flagged()
                 if self.field_object.count_remaining_tiles() <= 0:  # checking the winning condition
                     self.change_menu_text(won=True)
@@ -161,6 +170,7 @@ class Game:
             print('Clicked New Game')
             self.obtain_a_new_field()
             self.change_menu_text()
+            self.initiate_game_features()
         elif self.another_button.collidepoint(pos):
             print('Another button')
         else:
@@ -171,102 +181,9 @@ class Game:
         self.change_menu_text(lost=True)
 
 
-class Field:
-    def __init__(self, max_bombs, dimensions):
-        self.dimensions = dimensions
-
-        if max_bombs <= dimensions * dimensions:
-            tiles = [self.Tile(if_bomb=False) for _ in range((dimensions*dimensions) - max_bombs)] + \
-                    [self.Tile(if_bomb=True) for _ in range(max_bombs)]
-            random.shuffle(tiles)
-            self.field = [tiles[i:i+dimensions] for i in range(0, len(tiles), dimensions)]
-        else:
-            self.field = [[self.Tile(if_bomb=True) for _ in range(dimensions)] for _ in range(dimensions)]
-
-        self.max_bombs = max_bombs
-        self.total_tiles = dimensions * dimensions
-        self.safe_tiles = self.total_tiles - max_bombs
-        self.opened_tiles = 0
-
-    def get(self):
-        changed_field = self.field
-        for y in range(self.dimensions):
-            for x in range(self.dimensions):
-                if not changed_field[y][x].if_bomb:
-                    changed_field[y][x].bombs_around = self.count_bombs_around(x, y)
-        return changed_field
-
-    def count_bombs_around(self, x, y):
-        counted_bombs = 0
-        for i in range(-1, 2):  # this stuff generates the numbers -1, 0 and 1 to access adjacent cells
-            for j in range(-1, 2):
-                nx, ny = x + i, y + j
-                if 0 <= nx < self.dimensions and 0 <= ny < self.dimensions:
-                    if self.field[ny][nx].if_bomb:
-                        counted_bombs += 1
-        return counted_bombs
-
-    def open_tile(self, x, y, draw_object, screen):
-        if not (0 <= x < self.dimensions and 0 <= y < self.dimensions):
-            return  # the empty RETURN is a must here. It stops the execution of recursion.
-        tile = self.field[y][x]
-        if tile.if_open or tile.if_bomb:
-            return
-        tile.open_tile()
-
-        sleep(0.0000001)
-        draw_object(screen)
-
-        pygame.display.update()
-
-        self.opened_tiles += 1
-        if tile.is_totally_empty():  # checking and opening adjacent
-            for i in range(-1, 2):
-                for j in range(-1, 2):
-                    if j != 0 or i != 0:
-                        self.open_tile(x + i, y + j, draw_object, screen)
-
-    def open_all_tiles(self):
-        for row in self.field:
-            for tile in row:
-                tile.if_open = True
-
-    def count_remaining_tiles(self):
-        return self.total_tiles - self.opened_tiles - self.max_bombs
-
-    def __str__(self):
-        return str(self.field)
-
-    def __repr__(self):
-        return str(self.field)
-
-    class Tile:
-        def __init__(self, if_bomb):
-            self.bombs_around = 0
-            self.if_bomb = if_bomb
-            self.string = '*B*'
-            self.if_open = False
-            self.is_flagged = False
-
-        def is_totally_empty(self):
-            return not self.if_bomb and self.bombs_around == 0
-
-        def open_tile(self):
-            self.if_open = True
-
-        def get_flagged(self):
-            self.is_flagged = True if self.is_flagged is False else False
-
-        def __str__(self):
-            return self.string if self.if_bomb else str(self.bombs_around)
-
-        def __repr__(self):
-            return self.string if self.if_bomb else str(self.bombs_around)
-
-
 def main():
-    dim = 20
-    bombs = 50
+    dim = 38
+    bombs = int(38 * 38 * 0.2)
     Game(max_bombs=bombs, dimensions=dim, cell_size_pix=25)
 
 
